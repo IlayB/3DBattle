af1005e3050949ddead7b76c1b41435765493f55
IlayB <ilayb776@gmail.com>
2022-12-28 18:42:41 +0200

    fix: tests and index

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from "@testing-library/react";
-import App from "../App";
+import { render } from '@testing-library/react';
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText("Hello");
-  expect(linkElement).toBeInTheDocument();
+import App from '../App';
+
+describe('App', () => {
+  it('renders correctly', () => {
+    const { baseElement } = render(<App />);
+    expect(baseElement).toMatchSnapshot();
+  });
 });
