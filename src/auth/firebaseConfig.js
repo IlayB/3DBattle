47cb2e889d187e23d281384c52cd47541c9b19ce
IlayB <55955589+IlayB@users.noreply.github.com>
2023-07-20 14:44:37 +0300

    feat(85ztk0ykn): Add Firebase (#15)
    Issue: https://app.clickup.com/t/85ztk0ykn

---
+++ src/auth/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAPRPIoCHevKTJllkMwAerWhYquK6MRTN8',
+  authDomain: 'dbattle-de1da.firebaseapp.com',
+  projectId: 'dbattle-de1da',
+  storageBucket: 'dbattle-de1da.appspot.com',
+  messagingSenderId: '250496737996',
+  appId: '1:250496737996:web:5468684bf23d92692a5869',
+};
+
+export default firebaseConfig;
